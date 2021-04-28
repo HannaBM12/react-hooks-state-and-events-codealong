6fea5d6b4e15d90a7c3bd21af2d3f9b2a57661cc
HannaBM12 <Hannabmulugeta@gmail.com>
2021-04-28 19:07:21 -0400

    commit tree 2e812100521847a995c0a17d82dfca34584ed1a8

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const[isOn, setIsOn] = useState(false)
+
+  function handelClick(){
+      setIsOn(!isOn)
+  }
+  
+  const color = isOn? 'red' : 'white'
+  return <button style={{background: color}} onClick={handelClick}>{isOn ? "On" : "Off"}</button>;
 }
 
 export default Toggle;
+
+
+// function Toggle() {
+
+//   const[isOff, setIsOff] = useState(false)
+
+//   function handelClick(){
+//       setIsOff(!isOff)
+//   }
+
+//   return <button onClick={handelClick}>{isOff ? "On" : "Off"}</button>;
+// }
+
+// export default Toggle;
+
+
